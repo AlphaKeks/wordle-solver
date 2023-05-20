a7d3c9231df472b3857fa61159513c408fcfb8a1
AlphaKeks <alphakeks@dawn.sh>
2023-05-20 22:44:56 +0200

    feat: replace `matches` algorithm
    It turns out that the current implementation was not only way too slow,
    but also incorrect. Calculating the correctness between the current word
    and the candidate yields the correct result and is also a lot faster.
    
    Benchmark:
    
    ```
    hyperfine -N -r 5 -n "old \`matches\`" "./bins/wordle-solver-old-matches --games 10 --logs error --impl less-allocs" -n "new \`matches\`" "./bins/wordle-solver-new-matches --games 10 --logs error --impl less-allocs"
    ```
    
    Results:
    
    ```
    Benchmark 1: old `matches`
      Time (mean ± σ):     116.672 s ±  0.494 s    [User: 116.511 s, System: 0.006 s]
      Range (min … max):   115.994 s … 117.214 s    5 runs
    
    Benchmark 2: new `matches`
      Time (mean ± σ):     17.844 s ±  0.180 s    [User: 17.823 s, System: 0.007 s]
      Range (min … max):   17.638 s … 17.995 s    5 runs
    
    Summary
      'new `matches`' ran
        6.54 ± 0.07 times faster than 'old `matches`'
    ```

--- src/guesser.rs
+++ src/guesser.rs
@@ -24,86 +24,7 @@ impl Guess<'_> {
 		debug_assert_eq!(self.word.len(), 5);
 		debug_assert_eq!(cmp.len(), 5);
 
-		let mut marked = [false; 5];
-
-		// mark characters as `Correct`
-		let prev_guess = self.word.bytes();
-		let prev_correctness = self.correctness.iter();
-		let cmp_guess = cmp.bytes();
-
-		for (idx, ((prev_char, &prev_correctness), cmp_char)) in prev_guess
-			.zip(prev_correctness)
-			.zip(cmp_guess)
-			.enumerate()
-		{
-			// current character of the old word is marked as `Correct`
-			if prev_correctness.is_correct() {
-				// new word has different character -> the two don't match
-				// TODO: what about `Misplaced` characters?
-				if prev_char != cmp_char {
-					return false;
-				} else {
-					marked[idx] = true;
-				}
-			}
-		}
-
-		let prev_correctness = self.correctness.iter();
-		let cmp_guess = cmp.bytes();
-
-		for (idx, (cmp_char, &prev_correctness)) in
-			std::iter::zip(cmp_guess, prev_correctness).enumerate()
-		{
-			if prev_correctness.is_correct() {
-				continue;
-			}
-
-			let mut plausible = true;
-
-			let found_misplaced = self
-				.word
-				.bytes()
-				.zip(&self.correctness)
-				.enumerate()
-				.any(|(j, (char, correctness))| {
-					if char != cmp_char {
-						return false;
-					}
-
-					if marked[j] {
-						return false;
-					}
-
-					// We are looking for a character in `cmp`, and have found that character in our
-					// previous guess. The `correctness` of that previous character will tell us whether
-					// this new character _might_ be worth considering.
-					match correctness {
-						Correctness::Correct => {
-							unreachable!("All correct characters should have resulted in returning, or been marked.");
-						}
-						Correctness::Misplaced if idx == j => {
-							// `cmp_char` was `Misplaced` in the same position last time, which means that
-							// `cmp` _cannot_ possibly be the answer.
-							plausible = false;
-							false
-						}
-						Correctness::Misplaced => {
-							marked[j] = true;
-							true
-						}
-						Correctness::Incorrect => {
-							plausible = false;
-							false
-						}
-					}
-				});
-
-			if !found_misplaced && !plausible {
-				return false;
-			}
-		}
-
-		true
+		Correctness::compute(cmp, &self.word) == self.correctness
 	}
 }
 
@@ -143,6 +64,11 @@ mod tests {
 			check!("baaaa" + [I C M I I] disallows "caacc");
 			check!("baaaa" + [I C M I I] allows "aaccc");
 			check!("abcde" + [I I I I I] disallows "bcdea");
+		}
+
+		#[test]
+		fn caused_panic_lol() {
+			check!("tares" + [I M M I I] disallows "brink");
 		}
 	}
 }
